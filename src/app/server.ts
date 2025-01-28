582f0f68278a616026df6df5d5de7e30fad58ca7
ballll16 <ahmadhamidbalya@gmail.com>
2025-01-29 04:52:51 +0700

    refactor: 🦄 clean up

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,18 +1,40 @@
 import { Elysia } from 'elysia';
 import { controller } from './controller';
+import swagger from '@elysiajs/swagger';
+import node from '@elysiajs/node';
 
-const app = new Elysia();
-app.use(controller);
-app.onError(({ code, error }) => {
-  const res = {
-    errors: true,
-    message: 'Unhandled error',
-    data: null,
-  };
-  if (code === 'NOT_FOUND') {
-    res.message = error.message;
-    return res;
-  }
-});
+const app = new Elysia({ adapter: node() })
+  .use(controller)
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: 'Link3 API Spec',
+          description:
+            'Link3 is a simple link shortener built with Bun, Elysia, Prisma, and PostgreSQL.',
+          version: '1.0.0',
+        },
+        tags: [{ name: 'Links', description: 'Links' }],
+      },
+      path: '/ui',
+      scalarConfig: {
+        defaultHttpClient: {
+          targetKey: 'js' as 'javascript',
+          clientKey: 'axios',
+        },
+      },
+    }),
+  )
+  .onError(({ code, error }) => {
+    const res = {
+      errors: true,
+      message: 'Unhandled error',
+      data: null,
+    };
+    if (code === 'NOT_FOUND') {
+      res.message = error.message;
+      return res;
+    }
+  });
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
-import { Elysia } from 'elysia';
 import app from '@/server';
-import node from '@elysiajs/node';
-import swagger from '@elysiajs/swagger';
 import { BASE_URL } from './const';
 
-new Elysia({ adapter: node() })
-  .use(app)
-  .use(
-    swagger({
-      documentation: {
-        info: {
-          title: 'Link3 API Spec',
-          description:
-            'Link3 is a simple link shortener built with Bun, Elysia, Prisma, and PostgreSQL.',
-          version: '1.0.0',
-        },
-        tags: [{ name: 'Links', description: 'Links' }],
-      },
-      path: '/ui',
-      scalarConfig: {
-        defaultHttpClient: {
-          targetKey: 'js' as 'javascript',
-          clientKey: 'axios',
-        },
-      },
-    }),
-  )
-  .listen(3000, () => {
-    console.log(`🦊 Elysia is running at http://${BASE_URL}`);
-  });
+app.listen(3000, () => {
+  console.log(`🦊 Elysia is running at http://${BASE_URL}`);
+});
+
+export default app.fetch;
